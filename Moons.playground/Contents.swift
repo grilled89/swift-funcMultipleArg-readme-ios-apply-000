a58abd69a73719b796501c600a308f6ef576ee0a
Ryan King <rk13@Ryans-MacBook-Pro.local>
2017-01-06 21:37:05 -0500

    Hockey Stuff

--- Moons.playground/Contents.swift
+++ Moons.playground/Contents.swift
@@ -1,43 +1,10 @@
-func moonsOfJupiter() {
-    print("There are 67 moons orbiting Jupiter.")
+
+func hockeyTeams(name: String, numberOfGoals: Int, ssnPoints: Int) {
+    print("The \(name) has \(numberOfGoals) this season, and are in \(ssnPoints) place.")
+
 }
+hockeyTeams(name: "Islanders", numberOfGoals: 103, ssnPoints: 28)
 
-moonsOfJupiter()
+hockeyTeams(name: "Avalanche", numberOfGoals: 78, ssnPoints: 30)
+hockeyTeams(name: "Maple Leafs", numberOfGoals: 112, ssnPoints: 18)
 
-
-// planetDescription, take 1
-// The bit of code below is commented out, since we iterate on this definition of planetDescription() below.
-/*
-func planetDescription(planet: String) {
-    print("There are 67 moons orbiting \(planet).")
-}
-
-planetDescription(planet: "Jupiter")
-
-var planet = "Mars"
-
-planetDescription(planet: planet)
-*/
-
-
-func greet(name: String, greeting: String) {
-    print("\(greeting), \(name)!")
-}
-
-
-func planetDescription(planet: String, numberOfMoons: Int) {
-    print("There are \(numberOfMoons) moons orbiting \(planet)")
-}
-
-// This line of code gives an error.
-// Uncomment it to see what the error is!
-//planetDescription("Jupiter", 67)
-
-planetDescription(planet: "Jupiter", numberOfMoons: 67)
-
-var planet = "Jupiter"
-var moons = 67
-planetDescription(planet: planet, numberOfMoons: moons)
-planet = "Mars"
-moons = 2
-planetDescription(planet: planet, numberOfMoons: moons)
